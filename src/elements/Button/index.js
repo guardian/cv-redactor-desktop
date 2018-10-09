ea8a4e673ab8dc69ecd818a86233f50ca7369cfd
Laura <laura.gonzalez@guardian.co.uk>
2018-10-09 10:26:05 +0100

    nicer UI

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import styles from './index.css';
 
-export const Button = ({ children, onClick, type }) => (
-	<button className={styles.root} onClick={onClick} type={type}>
-		{children}
-	</button>
-);
+export const Button = ({ children, onClick, type, secondary }) => {
+	const className = secondary ? styles.secondary : styles.root;
+	return (
+		<button
+			className={className}
+			data-secondary={secondary}
+			onClick={onClick}
+			type={type}
+		>
+			{children}
+		</button>
+	);
+};
